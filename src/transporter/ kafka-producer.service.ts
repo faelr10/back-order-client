9fc6357d7affd2548deec44fa7e10af686f5d390
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-12-05 18:34:17 -0300

    🚧 implementing kafka-producer to project

---
+++ src/transporter/ kafka-producer.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { KafkaClient, Producer, ProduceRequest } from 'kafka-node';
+import { kafkaConfig } from './kafka.config';
+
+export type request_order_payload = {
+  origin: string;
+  order_id: string;
+  status: string;
+};
+
+@Injectable()
+export class KafkaProducerService {
+  private producer: Producer;
+
+  constructor() {
+    const client = new KafkaClient(kafkaConfig.client);
+    this.producer = new Producer(client);
+  }
+
+  send(topic: string, payload: request_order_payload): void {
+    const payloads: ProduceRequest[] = [
+      {
+        topic,
+        messages: JSON.stringify(payload),
+      },
+    ];
+
+    this.producer.send(payloads, (err, data) => {
+      if (err) {
+        console.error(`Erro ao enviar mensagem para o tópico ${topic}: ${err}`);
+      }
+    });
+  }
+}
---
+++ src/transporter/kafka.config.ts
@@ -0,0 +1,7 @@
+export const kafkaConfig = {
+  client: {
+    kafkaHost: 'localhost:9092',
+  },
+};
+
+export const request_order_topic = 'transaction_request.ledger.payment.1.0';
